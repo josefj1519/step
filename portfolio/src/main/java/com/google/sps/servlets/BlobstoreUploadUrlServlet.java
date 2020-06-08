fe970f6c3e0e9d47987311ff5301da981613dd6a
Josef Jankowski <jankowski.josef99@gmail.com>
2020-06-08 19:11:47 +0000

    Made servlet more RESTful

--- portfolio/src/main/java/com/google/sps/servlets/BlobstoreUploadUrlServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/BlobstoreUploadUrlServlet.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/blobstore-upload")
+@WebServlet("/blobstore-upload-url")
 public class BlobstoreUploadUrlServlet extends HttpServlet {
 
   @Override
