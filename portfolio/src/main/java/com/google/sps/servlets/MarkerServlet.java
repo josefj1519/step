c5f1cd000153b10f532b1623a0a3f57e3e26342f
Josef Jankowski <jankowski.josef99@gmail.com>
2020-06-11 17:18:36 +0000

    Remove marker class.

--- portfolio/src/main/java/com/google/sps/servlets/MarkerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/MarkerServlet.java
@@ -19,7 +19,6 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
-import com.google.sps.data.Marker;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
