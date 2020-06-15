e6816566a7b1fbf7d5975da6fb57c675ba049bc1
Josef Jankowski <jankowski.josef99@gmail.com>
2020-06-15 23:28:51 +0000

    Finished intro and query function

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,58 @@
 
 package com.google.sps;
 
+import java.util.Comparator;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+      Collection<TimeRange> queryResult = new ArrayList<>();
+            List<Event> eventsList = new ArrayList<>(events);
+      Collections.sort(eventsList, new Comparator<Event>() {
+            public int compare (Event e1, Event e2) {
+                return TimeRange.ORDER_BY_START.compare(e1.getWhen(), e2.getWhen());
+            }
+        });
+        eventsList.removeIf(e -> (
+        Collections.disjoint(e.getAttendees(), request.getAttendees()) || e.getWhen().duration() <= 0));
+      if( request.getAttendees().isEmpty()){
+          return Arrays.asList(TimeRange.WHOLE_DAY);
+      }
+      if(request.getDuration() > TimeRange.WHOLE_DAY.duration()){
+          return queryResult;
+      }
+      if(eventsList.size()==0){
+          return Arrays.asList(TimeRange.WHOLE_DAY);
+      }
+      if(eventsList.size()==1){
+          return Arrays.asList(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, eventsList.get(0).getWhen().start(), false ),
+          TimeRange.fromStartEnd(eventsList.get(0).getWhen().end(), TimeRange.END_OF_DAY, true)); 
+       }
+
+      for(int i=0;i<eventsList.size();i++){
+        if(i==0){
+            if(eventsList.get(i).getWhen().start()-request.getDuration()>=TimeRange.START_OF_DAY){
+              queryResult.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY,eventsList.get(i).getWhen().start(), false));
+            }
+        }
+        if(i != eventsList.size()-1){
+          if(eventsList.get(i).getWhen().overlaps(eventsList.get(i+1).getWhen())){
+            // Check for overlap.  Don't need to check for reverse, since it is pre-sorted.
+            if(eventsList.get(i).getWhen().contains(eventsList.get(i+1).getWhen())){
+                 eventsList.set(i+1, eventsList.get(i));
+            }   
+            // Middle   
+        } else if(eventsList.get(i+1).getWhen().start()-eventsList.get(i).getWhen().end()>=request.getDuration()){
+            queryResult.add(TimeRange.fromStartEnd(eventsList.get(i).getWhen().end(),eventsList.get(i+1).getWhen().start(), false));
+        } 
+       } else if (eventsList.get(i).getWhen().end()+request.getDuration()<= TimeRange.END_OF_DAY){
+           queryResult.add(TimeRange.fromStartEnd(eventsList.get(i).getWhen().end(), TimeRange.END_OF_DAY, true));
+       }
+      }
+      return queryResult;
   }
 }
