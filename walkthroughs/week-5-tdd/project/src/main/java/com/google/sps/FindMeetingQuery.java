3a19d0381e4e1a4a4b944503b7e0408f433ce7ab
Josef Jankowski <jankowski.josef99@gmail.com>
2020-06-19 20:26:55 +0000

    Add hasSufficentTime function.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,11 +14,13 @@
 
 package com.google.sps;
 
+
 import static java.util.Collections.EMPTY_LIST;
 import static java.util.Collections.disjoint;
 import static java.util.Collections.sort;
 import static java.util.Arrays.asList;
 
+import java.util.Optional;
 import java.util.Comparator;
 import java.util.Collection;
 import java.util.ArrayList;
@@ -68,34 +70,48 @@ public final class FindMeetingQuery {
   */
   private Collection<TimeRange> getOpenTimeSlots(List<Event> eventsList, MeetingRequest request){
       Collection<TimeRange> queryResult = new ArrayList<>();
+
+      Optional<TimeRange> optionalTimeRange;
+
+      optionalTimeRange = hasSufficientTime(TimeRange.START_OF_DAY, eventsList.get(0).getWhen().start(), request.getDuration(), false);
+      optionalTimeRange.ifPresent(range -> queryResult.add(range));
+      
       for(int i=0;i<eventsList.size();i++){
-         
+        TimeRange eventTime = eventsList.get(i).getWhen();
+        int eventEnd = eventTime.end();
         // First event in the list, check if there is enough time between event start and the start of the day. 
         //  If there is enough time, add to the result. Since the events are sorted by start time we can check the
         //  first index.
-        
-        if(i==0){
-            if(eventsList.get(i).getWhen().start()-request.getDuration()>=TimeRange.START_OF_DAY){
-              queryResult.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY,eventsList.get(i).getWhen().start(), false));
-            }
-        }
+
+        // If this isn't the last event.
         if(i != eventsList.size()-1){
-          if(eventsList.get(i).getWhen().overlaps(eventsList.get(i+1).getWhen())){
+          if(eventTime.overlaps(eventsList.get(i+1).getWhen())){
             // Check for overlap.  Don't need to check for the reverse, since it is pre-sorted. 
-            if(eventsList.get(i).getWhen().contains(eventsList.get(i+1).getWhen())){
+            if(eventTime.contains(eventsList.get(i+1).getWhen())){
                 //  If one event contains another event. Set i+1 to i.  
                 // This is to get the earliest start and latest end time for an event.
                  eventsList.set(i+1, eventsList.get(i));
             }   
         //  Check if there is enough time between two events. 
-        } else if(eventsList.get(i+1).getWhen().start()-eventsList.get(i).getWhen().end()>=request.getDuration()){
-            queryResult.add(TimeRange.fromStartEnd(eventsList.get(i).getWhen().end(),eventsList.get(i+1).getWhen().start(), false));
+        } else {
+            optionalTimeRange = hasSufficientTime(eventEnd, 
+                eventsList.get(i+1).getWhen().start(), request.getDuration(), false);
+            optionalTimeRange.ifPresent(range -> queryResult.add(range));
         } 
         // For last element in event list check if there is enough time between event end and end of day 
-       } else if (eventsList.get(i).getWhen().end()+request.getDuration()<= TimeRange.END_OF_DAY){
-           queryResult.add(TimeRange.fromStartEnd(eventsList.get(i).getWhen().end(), TimeRange.END_OF_DAY, true));
+       } else {
+           optionalTimeRange = hasSufficientTime(eventEnd,TimeRange.END_OF_DAY, 
+             request.getDuration(), true);
+           optionalTimeRange.ifPresent(range -> queryResult.add(range));
        }
       }
       return queryResult;
+  }
+
+  private Optional<TimeRange> hasSufficientTime(int start, int end, long duration, boolean inclusive){
+      if(end-start >= duration){
+          return Optional.of(TimeRange.fromStartEnd(start, end, inclusive));
+      }
+      return Optional.empty();
   }
 }
